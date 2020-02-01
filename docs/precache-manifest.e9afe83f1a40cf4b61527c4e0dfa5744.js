60da61c484519b968aa46129650a3a173d349d1c
RIKennedy <Ryan.Kennedy04@gmail.com>
2020-02-01 14:26:20 -0800

    Adding build to docs folder

---
+++ docs/precache-manifest.e9afe83f1a40cf4b61527c4e0dfa5744.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b1dd157ed23ff5fdcd072610af6503",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e80c600ad78460bf37e2",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "7fc8e0dfdbd88cc436c2",
+    "url": "/static/css/main.1361ba4d.chunk.css"
+  },
+  {
+    "revision": "e80c600ad78460bf37e2",
+    "url": "/static/js/2.d6fca685.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d6fca685.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7fc8e0dfdbd88cc436c2",
+    "url": "/static/js/main.6512faf1.chunk.js"
+  },
+  {
+    "revision": "e3f6cca2499cc0ccc243",
+    "url": "/static/js/runtime-main.b2109bab.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/static/media/SEGA.82e09f78.TTF"
+  },
+  {
+    "revision": "cdc09b4694b0efe2999f180a6cbbd990",
+    "url": "/static/media/SFTransRobotics.cdc09b46.ttf"
+  }
+]);
